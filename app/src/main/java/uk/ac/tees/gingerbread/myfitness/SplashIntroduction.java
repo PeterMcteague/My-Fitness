b78d0b44611d08bac177d69121a31f385f5937dd
Peter Mcteague <07mcteaguepet@googlemail.com>
2017-03-10 12:16:35 +0000

    Created splash screen intro
    Also added drawable for splash screen

--- app/src/main/java/uk/ac/tees/gingerbread/myfitness/SplashIntroduction.java
+++ app/src/main/java/uk/ac/tees/gingerbread/myfitness/SplashIntroduction.java
@@ -1,7 +1,11 @@
 package uk.ac.tees.gingerbread.myfitness;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
 
 public class SplashIntroduction extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class SplashIntroduction extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_introduction);
+
+        // Get context and button objects
+        final Context context = this;
+        Button button = (Button) findViewById(R.id.splash_intro_next_button);
+
+        // Add next button event listener
+        button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(context, SplashPersonal.class);
+                        startActivity(intent);
+                    }
+                }
+
+        );
     }
 }
