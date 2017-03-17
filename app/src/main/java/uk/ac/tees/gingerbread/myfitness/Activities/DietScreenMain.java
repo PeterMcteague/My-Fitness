7527f31d8a501e8b8ef5e6c2a328580cc7c900ec
Peter Mcteague <07mcteaguepet@googlemail.com>
2017-03-17 15:08:52 +0000

    Updated dietscreen code

--- app/src/main/java/uk/ac/tees/gingerbread/myfitness/Activities/DietScreenMain.java
+++ app/src/main/java/uk/ac/tees/gingerbread/myfitness/Activities/DietScreenMain.java
@@ -4,8 +4,11 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Calendar;
+
 import uk.ac.tees.gingerbread.myfitness.DatabaseHandling.DatabaseHandler;
 import uk.ac.tees.gingerbread.myfitness.R;
+import uk.ac.tees.gingerbread.myfitness.Classes.DietEntry;
 
 public class DietScreenMain extends AppCompatActivity {
 
@@ -14,19 +17,30 @@ public class DietScreenMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet_screen_main);
 
-        //Create record for diet in db if there isn't one for today
-        final Context context = this;
-        DatabaseHandler dh = new DatabaseHandler(context);
-        if (selectedDate == today)
-        {
-            if (dh.getDietEntryToday() == null)
-            {
-                dh.addDietEntryToday(TODO: HERE);
-            }
-            //If date is today
-            //Update activity to add calories and protein
-        }
-        //Load goal and protein for day using diet entry
+//        //Calendar
+//        final Calendar c = Calendar.getInstance();
+//        c.set(Calendar.HOUR_OF_DAY, 0);
+//        c.set(Calendar.MINUTE, 0);
+//        c.set(Calendar.SECOND, 0);
+//        c.set(Calendar.MILLISECOND, 0);
+//
+//        //Create record for diet in db if there isn't one for today
+//        final Context context = this;
+//        DatabaseHandler dh = new DatabaseHandler(context);
+//        if (selectedDate == c.getTimeInMillis())
+//        {
+//            if (dh.getDietEntryToday() == null)
+//            {
+//                dh.addDietEntryToday();
+//            }
+//        }
+//        //Set activity attributes
+//        DietEntry diet = dh.getDietEntryToday();
+//        diet.getCalories();
+//        diet.getCaloriesGoal();
+//        diet.getProtein();
+//        diet.getProteinGoal();
+
     }
 }*/
 }
