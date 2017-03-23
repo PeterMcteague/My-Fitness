28268d61966affd80281c962e4c05a4b72d0415a
Peter Mcteague <07mcteaguepet@googlemail.com>
2017-03-23 19:56:07 +0000

    Updated diet screen
    Needs testing but this should look better aesthetically and function
    better.

--- app/src/main/java/uk/ac/tees/gingerbread/myfitness/Activities/DietScreenMain.java
+++ app/src/main/java/uk/ac/tees/gingerbread/myfitness/Activities/DietScreenMain.java
@@ -3,12 +3,15 @@ package uk.ac.tees.gingerbread.myfitness.Activities;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
@@ -16,6 +19,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.loopj.android.http.JsonHttpResponseHandler;
 
@@ -41,7 +45,7 @@ public class DietScreenMain extends AppCompatActivity {
     private long todayTimeInMillis;
     private final DatabaseHandler dh = new DatabaseHandler(this);
     private DietEntry diet;
-    private Spinner foodList;
+    private ListView foodList;
     private final Activity activity = DietScreenMain.this;
 
     private void getEntries(final Activity activity, String searchQuery) {
@@ -63,11 +67,31 @@ public class DietScreenMain extends AppCompatActivity {
                             }
                         }
 
-                        //Use nutritionix adapter here by updating a listview and setting adapter.
-                        foodList = (Spinner) findViewById(R.id.spinner_food_choice);
-                        nutritionixAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        //Use nutritionix to populate list view with buttons
+                        foodList = (ListView) findViewById(R.id.diet_food_list);
                         foodList.setAdapter(nutritionixAdapter);
-                        foodList.setVisibility(View.VISIBLE);
+                        foodList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position,
+                                                    long id) {
+                                final NutritionixModel selected = (NutritionixModel) foodList.getItemAtPosition(position);
+
+                                new AlertDialog.Builder(activity)
+                                        .setTitle("Add " + selected.getName() + " to diet?")
+                                        .setMessage("Do you want to add " + selected.getName() + " to your goals?")
+                                        .setIcon(android.R.drawable.ic_dialog_alert)
+                                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                                            public void onClick(DialogInterface dialog, int whichButton) {
+                                                diet.setCalories(diet.getCalories() + selected.getCalories());
+                                                diet.setProtein(diet.getProtein() + selected.getProtein());
+                                                dh.updateDietEntry(diet,timeInMillis);
+                                                updateTextFields(diet);
+                                                Toast.makeText(activity, "Added calories and/or protein to total.", Toast.LENGTH_LONG).show();
+                                            }})
+                                        .setNegativeButton(android.R.string.no, null).show();
+                            }
+                        });
                     }});}
 
     /**Updates text fields on the screen with info from a diet object
@@ -76,37 +100,38 @@ public class DietScreenMain extends AppCompatActivity {
      */
     protected void updateTextFields(DietEntry diet)
     {
-        //Update text fields
-        //Set calories and calories goal
+        EditText currentCaloriesView = (EditText) findViewById(R.id.diet_calories_entry);
+        TextView goalCaloriesView = (TextView) findViewById(R.id.diet_calories_goal_text);
+        TextView caloriesTextView = (TextView) findViewById(R.id.diet_calories_text);
+
+        EditText currentProteinView = (EditText) findViewById(R.id.diet_protein_entry);
+        TextView goalProteinView = (TextView) findViewById(R.id.diet_protein_goal_text);
+        TextView proteinTextView = (TextView) findViewById(R.id.diet_protein_text);
+
+
         if (diet.getCaloriesGoal() != 0)
         {
-            TextView currentCaloriesView = (TextView) findViewById(R.id.calories_current);
-            TextView goalCaloriesView = (TextView) findViewById(R.id.calories_goal);
             currentCaloriesView.setText(Integer.toString(diet.getCalories()));
             goalCaloriesView.setText(Integer.toString(diet.getCaloriesGoal()));
         }
         else
         {
-            TextView currentCaloriesView = (TextView) findViewById(R.id.calories_current);
-            TextView goalCaloriesView = (TextView) findViewById(R.id.calories_goal);
             currentCaloriesView.setVisibility(View.GONE);
             goalCaloriesView.setVisibility(View.GONE);
+            caloriesTextView.setVisibility(View.GONE);
         }
 
         //Same for protein
         if (diet.getProteinGoal() != 0)
         {
-            TextView currentProteinView = (TextView) findViewById(R.id.protein_current);
-            TextView goalProteinView = (TextView) findViewById(R.id.protein_goal);
             currentProteinView.setText(Float.toString(diet.getProtein()));
             goalProteinView.setText(Float.toString(diet.getProteinGoal()));
         }
         else
         {
-            TextView currentProteinView = (TextView) findViewById(R.id.protein_current);
-            TextView goalProteinView = (TextView) findViewById(R.id.protein_goal);
             currentProteinView.setVisibility(View.GONE);
             goalProteinView.setVisibility(View.GONE);
+            proteinTextView.setVisibility(View.GONE);
         }
     }
 
@@ -115,7 +140,8 @@ public class DietScreenMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diet_screen_main);
 
-        Button dateButton = (Button)findViewById(R.id.btn_date);
+        //DATE BUTTON
+        Button dateButton = (Button)findViewById(R.id.diet_date_button);
         //Set variables
         final Calendar c = Calendar.getInstance();
         c.set(Calendar.HOUR_OF_DAY, 0);
@@ -125,7 +151,6 @@ public class DietScreenMain extends AppCompatActivity {
         timeInMillis = c.getTimeInMillis();
         todayTimeInMillis = c.getTimeInMillis();
 
-        //Button should change calendar and set time in millis
         dateButton.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
@@ -137,14 +162,14 @@ public class DietScreenMain extends AppCompatActivity {
                                 {
                                     //On date set read diet entry from db for selected day and set timeinmillis
                                     c.set(year, monthOfYear, dayOfMonth);
-                                    diet = dh.getDietEntry(c.getTimeInMillis());
                                     timeInMillis = c.getTimeInMillis();
-
+                                    diet = dh.getDietEntry(timeInMillis);
                                     updateTextFields(diet);
                                 }
                                 }, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
                         datePickerDialog.show();
                 }});
+
 
         //Create record for diet in db if there isn't one for today
         if (dh.getDietEntryToday() == null)
@@ -156,87 +181,53 @@ public class DietScreenMain extends AppCompatActivity {
         diet = dh.getDietEntryToday();
         updateTextFields(diet);
 
-        //Add updater choices to spinner
-        final Spinner updateChoice = (Spinner) findViewById(R.id.update_choice_spinner);
-        List<String> choiceList = new ArrayList<>();
-        choiceList.add("Calories");
-        choiceList.add("Protein");
-        choiceList.add("Food");
-        ArrayAdapter<String> dataAdapterActivity = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, choiceList);
-        dataAdapterActivity.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        updateChoice.setAdapter(dataAdapterActivity);
+        //Bind EditText's
+        EditText currentCaloriesView = (EditText) findViewById(R.id.diet_calories_entry);
+        EditText currentProteinView = (EditText) findViewById(R.id.diet_protein_entry);
+        EditText foodEntryView = (EditText) findViewById(R.id.diet_food_entry);
 
-        updateChoice.setOnClickListener(new View.OnClickListener()
-        {
+        currentCaloriesView.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View v)
-            {
-                //Find value of spinner
-                String selection = updateChoice.getSelectedItem().toString();
-                if (selection == "Food")
-                {
-                    //Set the food chooser to visible
-                    foodList = (Spinner) findViewById(R.id.spinner_food_choice);
-                    foodList.setVisibility(View.VISIBLE);
-                }
-            }
-        });
-
-        //Bind update button
-        Button updateButton = (Button) findViewById(R.id.update_button);
-        final EditText textField = (EditText) findViewById(R.id.value_entry_field);
-
-        textField.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                //Don't care about this
-            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                //If nutritionix selected, send a query with the text
-                getEntries(activity,s.toString());
-            }
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
             @Override
             public void afterTextChanged(Editable s) {
-                //Don't care about this
+                diet.setCalories(Integer.parseInt(s.toString()));
+                dh.updateDietEntry(diet,timeInMillis);
+                Toast.makeText(getApplicationContext(),"Calories updated.",Toast.LENGTH_SHORT);
             }
         });
 
-        updateButton.setOnClickListener(
-                new View.OnClickListener()
-                {
-                    @Override
-                    public void onClick(View v)
-                    {
-                        //Find value of spinner
-                        String selection = updateChoice.getSelectedItem().toString();
-                        if (selection == "Calories")
-                        {
-                            //Get current calories, add value in box and update entry and refresh.
-                            diet.setCalories(diet.getCalories()+ Integer.parseInt(textField.getText().toString()));
-                            dh.updateDietEntry(diet , timeInMillis);
-                            updateTextFields(diet);
-                        }
-                        else if (selection == "Protein")
-                        {
-                            //Get current protein, add value in box and update entry and refresh.
-                            diet.setProtein(diet.getProtein() + Long.parseLong(textField.getText().toString()));
-                            dh.updateDietEntry(diet , timeInMillis);
-                            updateTextFields(diet);
-                        }
-                        else if (selection == "Food")
-                        {
-                            //Ask nurtitionix for calories and protein KEY 973127408431e443f91406c6aa837715 DOC https://developer.nutritionix.com/v1_1/quick-start/simple-food-search
-                            diet.setProtein(diet.getProtein() + (((NutritionixModel) foodList.getSelectedItem()).getProtein()));
-                            diet.setCalories(diet.getCalories()+ (((NutritionixModel) foodList.getSelectedItem()).getCalories()));
-                            dh.updateDietEntry(diet , timeInMillis);
-                            updateTextFields(diet);
-                        }
-                    }
-                }
-        );
+        currentProteinView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                diet.setProtein(Integer.parseInt(s.toString()));
+                dh.updateDietEntry(diet,timeInMillis);
+                Toast.makeText(getApplicationContext(),"Protein updated.",Toast.LENGTH_SHORT);
+            }
+        });
+
+        foodEntryView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                getEntries(activity,s.toString());
+            }
+        });
     }
 }
