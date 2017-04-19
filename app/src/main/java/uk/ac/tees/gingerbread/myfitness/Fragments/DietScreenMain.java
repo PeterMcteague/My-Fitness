0cbea54bd467375bda841af375e7069fe879a732
Peter Mcteague <07mcteaguepet@googlemail.com>
2017-04-19 20:24:22 +0100

    Made edittexts more consistent on info screen
    The edittexts are now more consistent. Have broken the floating action
    button, will fix

--- app/src/main/java/uk/ac/tees/gingerbread/myfitness/Fragments/DietScreenMain.java
+++ app/src/main/java/uk/ac/tees/gingerbread/myfitness/Fragments/DietScreenMain.java
@@ -4,13 +4,9 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.support.annotation.Nullable;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.text.Editable;
-import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -18,7 +14,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -141,7 +136,7 @@ public class DietScreenMain extends Fragment {
     @Override
     //Include any getting of views in here and assign to variables.
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_diet_screen_main, container,false);
+        View view = inflater.inflate(R.layout.fragment_diet, container,false);
 
         setHasOptionsMenu(true);
 
